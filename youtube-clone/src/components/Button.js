7a09ef1bd721fba0ef2116c6bf220f7c573a651b
priyanshusingh92 <singhpriyanshu772@gmail.com>
2023-04-04 11:29:17 +0530

    fixing icon

--- youtube-clone/src/components/Button.js
+++ youtube-clone/src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({name}) => {
 
   return (
-    <button className='mx-2 p-2 bg-gray-200 rounded-xl'>{name}</button>
+    <button className='mx-2 p-2 bg-gray-200 rounded-xl py-2'>{name}</button>
   )
 }
 
