f46922d617ba4645cb7f26da33df0ce5e474566c
priyanshusingh92 <singhpriyanshu772@gmail.com>
2023-04-04 12:16:52 +0530

    fixing search api

--- youtube-clone/src/utils/constants.js
+++ youtube-clone/src/utils/constants.js
@@ -1,6 +1,6 @@
 const GOOGLE_API_KEY = "AIzaSyDhWFwpUxQecoQ3RHT1jjVwgxj7V2O44iA";
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 export const VIDEOS_LIST_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
   GOOGLE_API_KEY;
